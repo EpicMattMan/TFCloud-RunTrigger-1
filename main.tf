73b29f61f8aba11a34c7f10de3c719c6fe12af9e
EpicMattMan <66376412+EpicMattMan@users.noreply.github.com>
2022-02-09 14:16:00 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-data "terraform_remote_state" "locaiton" {
+data "terraform_remote_state" "region" {
   backend = "remote"
 
   config = {
@@ -6,5 +6,29 @@ data "terraform_remote_state" "locaiton" {
     workspaces = {
           name = "TFCloud-TriggerDeploy"
     }
+  }
+}
+
+provider "azurerm" {
+  version =  "2.66.0"
+
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.clientSecret
+  tenant_id = var.tenant_id
+
+  features{}
+
+  
+resource "azurerm_storage_account" "sa" {
+  name                     = "tfcdiagnosticstore1191" 
+  resource_group_name      = var.rgName
+  location                 = data.terraform_remote_state.region.outputs.location
+   account_tier            = "Standard"
+   account_replication_type = "LRS"
+
+   tags = {
+    environment = "TFC test"
+   }
   }
 }
